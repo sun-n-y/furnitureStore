774b454c49619b680e16d0bcad30bf22f8c531d6
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-19 16:04:23 -0500

    add error page structure

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,36 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <main className="grid min-h-[100vh] place-items-center px-8 ">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page Not Found
+          </h1>
+          <p className="mt-6 text-lg leading-7">
+            Sorry, we couldn't find the page you're looking for.
+          </p>
+          <div className="mt-10">
+            <Link to="/" className="btn btn-secondary">
+              GO BACK HOME
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="grid min-h-[100vh] place-items-center px-8 ">
+      <h4 className="text-center font-bold text-4xl">
+        there was an error...{error.message}
+      </h4>
+    </main>
+  );
 };
 export default Error;
