7dd484b463cf702d7f07a58a48dafd9561c0f326
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-19 15:44:29 -0500

    add route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import {
   About,
   Cart,
@@ -12,7 +13,55 @@ import {
   SingleProduct,
 } from './pages';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1>HomeLayout</h1>;
+  return (
+    <>
+      <nav>
+        <span className="text-4xl text-primary">comfy</span>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 export default HomeLayout;
