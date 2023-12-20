577bf8c3f85a3a45558e4b147c4326c9d8e5902f
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-20 11:53:33 -0500

    add submit btn structure

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,8 +1,24 @@
+import { useNavigation } from 'react-router-dom';
+
 const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
-    <button type="submit" className="btn btn-primary">
-      {text}
+    <button
+      type="submit"
+      className="btn btn-primary btn-block"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner">sending...</span>
+        </>
+      ) : (
+        text || 'submit'
+      )}
     </button>
   );
 };
+
 export default SubmitBtn;
