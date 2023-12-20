a9f5b0e5c9e1d21dd5900be9b5c325fd56c391e1
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-20 12:10:07 -0500

    add register page structure

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,31 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
+
 const Register = () => {
-  return <h1>Register</h1>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput type="text" label="username" name="username" />
+        <FormInput type="text" label="email" name="email" />
+        <FormInput type="text" label="password" name="password" />
+        <div className="mt-4">
+          <SubmitBtn text="REGISTER" />
+        </div>
+        <p className="text-center">
+          Already a member?
+          <Link
+            to="/login"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            Login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Register;
