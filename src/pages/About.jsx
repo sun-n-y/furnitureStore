0d9a005186567d90f5d408578dab3657de3c38ee
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-22 17:26:25 -0500

    add about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,26 @@
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia
+        odit aut dignissimos optio atque eius quas esse voluptatum saepe
+        consectetur, incidunt repellat officia, nulla iste tempore corporis
+        ratione quaerat.
+      </p>
+    </>
+  );
 };
+
 export default About;
