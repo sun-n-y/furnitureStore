8068b426e788943f82888a39631b4cb35ed2ac7f
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-27 11:40:20 -0500

    add select inputs

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,22 +1,46 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
       <FormInput
         type="search"
-        label="search product"
+        label="Search Products"
         name="search"
         size="input-sm"
       />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="Select Category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="Select Company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* SORT BY */}
+      <FormSelect
+        label="Sort By"
+        name="order"
+        list={['a-z', 'z-a', 'high', 'low']}
+        size="select-sm"
+      />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
-        search
+        Search
       </button>
       <Link to="/products" className="btn btn-accent btn-sm">
-        reset
+        Reset
       </Link>
     </Form>
   );
