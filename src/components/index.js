aa8b7977c46def24711d1d76861f528518b05cbc
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-28 12:01:06 -0500

    add global loading component

--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,4 @@ export { default as Filters } from './Filters';
 export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
 export { default as ProductsList } from './ProductsList';
+export { default as Loading } from './Loading';
