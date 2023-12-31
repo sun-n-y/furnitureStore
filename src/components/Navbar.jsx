a3137bdb3f40520fb80e89213606396677aca41e
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-30 21:22:30 -0500

    add more logic to add item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const themes = {
   winter: 'winter',
@@ -26,6 +27,10 @@ const Navbar = () => {
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
   }, [theme]);
+
+  const numItemsInCart = useSelector((state) => {
+    return state.cartState.numItemsInCart;
+  });
 
   return (
     <nav className="bg-base-200">
@@ -70,7 +75,7 @@ const Navbar = () => {
             <div className="indicator">
               <BsCart3 className="h-6 w-6" />
               <span className="badge badge-sm badge-primary indicator-item ">
-                8
+                {numItemsInCart}
               </span>
             </div>
           </NavLink>
