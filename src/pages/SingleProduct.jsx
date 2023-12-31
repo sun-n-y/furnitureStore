9048d987c0bdd9c0d8316b25139da110a3fdbc72
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-30 20:14:37 -0500

    add cart functionlaity

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,6 +1,8 @@
 import { Link, useLoaderData } from 'react-router-dom';
 import authFetch, { formatPrice, generateAmountOptions } from '../utils/index';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../features/cart/cartSlice';
 
 export const loader = async ({ params }) => {
   const response = await authFetch(`/products/${params.id}`);
@@ -18,6 +20,23 @@ const SingleProduct = () => {
 
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value));
+  };
+
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    company,
+    productColor,
+    amount,
+  };
+
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(addItem({ product: cartProduct }));
   };
 
   return (
@@ -87,10 +106,7 @@ const SingleProduct = () => {
           </div>
           {/* CART BTN */}
           <div className="mt-10">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => console.log('add to bag')}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to bag
             </button>
           </div>
