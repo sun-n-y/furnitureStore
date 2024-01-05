29a0b73ca8a9fb130fd5d7ed42c455b286a812e7
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-05 12:35:27 -0500

    add register action

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { loader as landingLoader } from './pages/Landing';
 import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productsLoader } from './pages/Products';
 // actions
+import { action as registerAction } from './pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -71,6 +72,7 @@ const router = createBrowserRouter([
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
