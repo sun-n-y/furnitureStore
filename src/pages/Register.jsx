a92ac3083feb5529b0c98e672ca878778566c264
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-05 13:00:23 -0500

    add register action complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,25 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
+import authFetch from '../utils';
+import { toast } from 'react-toastify';
 
-export const action = async () => {
-  return null;
+export const action = async ({ request }) => {
+  console.log(request);
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  try {
+    const response = await authFetch.post('/auth/local/register', data);
+    toast.success('account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
