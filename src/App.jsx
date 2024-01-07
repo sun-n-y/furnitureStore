467015c36f893554f9d999b3cd83be127bf9acb2
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-07 12:02:21 -0500

    add restrict acces to checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { store } from './store';
 import { loader as landingLoader } from './pages/Landing';
 import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
@@ -54,6 +55,7 @@ const router = createBrowserRouter([
       {
         path: 'checkout',
         element: <Checkout />,
+        loader: checkoutLoader(store),
       },
       {
         path: 'orders',
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,17 @@
 import { useSelector } from 'react-redux';
 import { CartTotals, CheckoutForm, SectionTitle } from '../components';
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+export const loader = (store) => () => {
+  //grab user from the store
+  const user = store.getState().userState.user;
+  if (!user) {
+    toast.warning('You must be logged in to checkout');
+    return redirect('/login');
+  }
+  return null;
+};
 
 const Checkout = () => {
   const cartItems = useSelector((state) => state.cartState.cartTotal);
