13370177dd7107b1278523b400450bbfed5590fe
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-08 14:39:09 -0500

    add checkout form action

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,15 +1,54 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
 import FormInput from './FormInput';
 import SubmitBtn from './SubmitBtn';
+import authFetch, { formatPrice } from '../utils';
+import { toast } from 'react-toastify';
+import { clearCart } from '../features/cart/cartSlice';
 
-export const action = (store) => async () => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+
+    try {
+      const response = await authFetch.post(
+        '/orders',
+        { data: info },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      store.dispatch(clearCart());
+      toast.success('order placed successfully');
+      return redirect('/orders');
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'There was an error placing your order';
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const CheckoutForm = () => {
   return (
-    <Form method="POST" className="felx flex-col gap-y-4">
+    <Form method="POST" className="flex flex-col gap-y-4">
       <h4 className="font-medium text-xl">Shipping Information</h4>
       <FormInput label="First Name" name="name" type="text" />
       <FormInput label="Address" name="address" type="text" />
