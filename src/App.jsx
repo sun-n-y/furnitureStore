bcc339e1ae73fd1a4da830e60a85f3d92bf427e2
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-08 12:44:58 -0500

    add checkout form setup

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import { loader as checkoutLoader } from './pages/Checkout';
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
 
 const router = createBrowserRouter([
   {
@@ -56,6 +57,7 @@ const router = createBrowserRouter([
         path: 'checkout',
         element: <Checkout />,
         loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: 'orders',
--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,5 +1,23 @@
+import { Form } from 'react-router-dom';
+import FormInput from './FormInput';
+import SubmitBtn from './SubmitBtn';
+
+export const action = (store) => async () => {
+  console.log(store);
+  return null;
+};
+
 const CheckoutForm = () => {
-  return <h1>CheckoutForm</h1>;
+  return (
+    <Form method="POST" className="felx flex-col gap-y-4">
+      <h4 className="font-medium text-xl">Shipping Information</h4>
+      <FormInput label="First Name" name="name" type="text" />
+      <FormInput label="Address" name="address" type="text" />
+      <div className="mt-4">
+        <SubmitBtn text="PLACE YOUR ORDER" />
+      </div>
+    </Form>
+  );
 };
 
 export default CheckoutForm;
