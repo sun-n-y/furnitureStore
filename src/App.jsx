a7a97206fb787266fff33469d49b183b218f70cb
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-09 15:40:05 -0500

    add order get http

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import { loader as landingLoader } from './pages/Landing';
 import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productsLoader } from './pages/Products';
 import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
@@ -62,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: 'orders',
         element: <Orders />,
+        loader: ordersLoader(store),
       },
       {
         path: 'about',
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,39 @@
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import authFetch from '../utils';
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user;
+    if (!user) {
+      toast.warning('You must be logged in to see orders');
+      return redirect('/login');
+    }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await authFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'There was an error fetching your order';
+      toast.error(errorMessage);
+      if (error.response.status === 401 || 403) return redirect('/login');
+      return null;
+    }
+  };
+
 const Orders = () => {
   return <h1>Orders</h1>;
 };
+
 export default Orders;
